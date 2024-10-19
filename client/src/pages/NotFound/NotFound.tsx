615f062cefb23e1107df4622810fb442eabc2272
toluwalasewasehere <haastrup.toluwalase@gmail.com>
2024-10-19 11:27:55 -0400

    Add Not Found

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -5,6 +5,7 @@ function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
       <h1>404</h1>
+      <img src="404-Page-Featured-Image" alt="404 Image" width = "500" height= "500"></img>
     </Layout>
   );
 }
